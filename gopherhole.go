6370947e256edfa1b3dda91ef539a726a79c9990
christian-westbrook <christianwestbrook@live.com>
2025-02-07 21:37:35 -0600

    Working example of parsing XML data

--- gopherhole.go
+++ gopherhole.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 )
@@ -22,15 +23,37 @@ func main() {
 				<LastName>Smith</LastName>
 				<DateOfBirth>1992-03-22</DateOfBirth>
 			</Patient>
+
 		</Patients>
 	`)
 
-	// Map unmarshaled XML tags to their values
-	var tagValueMap map[string]interface{}
+	// Create an XML decoder
+	xmlDataReader := bytes.NewReader(xmlData)
+	decoder := xml.NewDecoder(xmlDataReader)
 
-	err := xml.Unmarshal(xmlData, &tagValueMap)
+	// Map for storing unmarshaled XML tags and their values
+	//var tagValueMap map[string]interface{}
 
-	if err != nil {
-		fmt.Println("Error unmarshaling XML:", err)
+	// Iterate over tokens in the decoder
+	for {
+
+		// Unpack the next token
+		token, error := decoder.Token()
+
+		if error != nil {
+			break
+		}
+
+		// Switch on the token's asserted type
+		switch t := token.(type) {
+		case xml.StartElement:
+			fmt.Println("Start of element:", t.Name.Local)
+		case xml.CharData:
+			fmt.Println("Payload of element:", string(t))
+		case xml.EndElement:
+			fmt.Println("End of element:", t.Name.Local)
+		default:
+			fmt.Println("Unhandled token encountered")
+		}
 	}
 }
