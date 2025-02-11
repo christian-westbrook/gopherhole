c242f5b00a9852e4005c1d94250fb4601e52daa1
christian-westbrook <christianwestbrook@live.com>
2025-02-10 20:15:13 -0600

    Moved the application introduction to a dedicated function

--- gopherhole.go
+++ gopherhole.go
@@ -10,33 +10,7 @@ import (
 
 func main() {
 
-	// Introduction
-	fmt.Println()
-	fmt.Println("Welcome to the Gopher Hole v0.1.0!")
-	fmt.Println()
-	fmt.Println("Throw your XML into the hole, and the")
-	fmt.Println("Gophers will toss back JSON!")
-	fmt.Println()
-
-	fmt.Println("         ,_---~~~~~----._         ")
-	fmt.Println("  _,,_,*^____      _____``*g*\"*, ")
-	fmt.Println(" / __/ /'     ^.  /      \\ ^@q   f ")
-	fmt.Println("[  @f | @))    |  | @))   l  0 _/  ")
-	fmt.Println(" \\`/   \\~____ / __ \\_____/    \\   ")
-	fmt.Println(" |           _l__l_           I   ")
-	fmt.Println(" }          [______]           I  ")
-	fmt.Println(" ]            | | |            |  ")
-	fmt.Println(" ]             ~ ~             |  ")
-	fmt.Println(" |                            |   ")
-	fmt.Println("  |                           |   ")
-	fmt.Println()
-	fmt.Println("Developed by Christian Westbrook ")
-	fmt.Println("https://github.com/christian-westbrook/")
-	fmt.Println()
-
-	fmt.Println("Artwork by belbomemo")
-	fmt.Println("https://gist.github.com/belbomemo")
-	fmt.Println()
+	intro()
 
 	// Example of input XML data
 	xmlData := []byte(`
@@ -115,6 +89,37 @@ func main() {
 			fmt.Println("Unhandled token encountered")
 		}
 	fmt.Println(configMap["patients"].([]interface{})[0].(map[string]interface{})["age"])
+}
+
+// Introduce the application
+func intro() {
+	// Introduction
+	fmt.Println()
+	fmt.Println("Welcome to the Gopher Hole v0.1.0!")
+	fmt.Println()
+	fmt.Println("Throw your XML into the hole, and the")
+	fmt.Println("Gophers will toss back JSON!")
+	fmt.Println()
+
+	fmt.Println("         ,_---~~~~~----._         ")
+	fmt.Println("  _,,_,*^____      _____``*g*\"*, ")
+	fmt.Println(" / __/ /'     ^.  /      \\ ^@q   f ")
+	fmt.Println("[  @f | @))    |  | @))   l  0 _/  ")
+	fmt.Println(" \\`/   \\~____ / __ \\_____/    \\   ")
+	fmt.Println(" |           _l__l_           I   ")
+	fmt.Println(" }          [______]           I  ")
+	fmt.Println(" ]            | | |            |  ")
+	fmt.Println(" ]             ~ ~             |  ")
+	fmt.Println(" |                            |   ")
+	fmt.Println("  |                           |   ")
+	fmt.Println()
+	fmt.Println("Developed by Christian Westbrook ")
+	fmt.Println("https://github.com/christian-westbrook/")
+	fmt.Println()
+
+	fmt.Println("Artwork by belbomemo")
+	fmt.Println("https://gist.github.com/belbomemo")
+	fmt.Println()
 }
 
 // Determine whether a given string is whitespace
